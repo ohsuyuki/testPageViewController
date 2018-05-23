774f1de024764ead6d5e71f50c694b4b62bf4e97
ohsuyuki <[ohsu@liquidinc.asia]>
2018-05-23 18:20:18 +0900

    create

--- testPageViewController/ViewController.swift
+++ testPageViewController/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+
+    var viewControllers: [UIViewController] = []
+    var pageViewController: UIPageViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        initViewControllers()
+        initPageViewController()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func initViewControllers() {
+        viewControllers = [
+            storyboard!.instantiateViewController(withIdentifier: "Page1"),
+            storyboard!.instantiateViewController(withIdentifier: "Page2"),
+        ]
+    }
+    
+    private func initPageViewController() {
+        pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pageViewController.dataSource = self
+        pageViewController.setViewControllers([viewControllers[0]], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+        view.addSubview(pageViewController.view)
     }
 
+    // UIPageViewControllerDataSource
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let index = viewControllers.index(of: viewController)
+        if index == 0 {
+            return nil
+        }
+        return viewControllers[index! - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let index = viewControllers.index(of: viewController)
+        if index == viewControllers.count - 1 {
+            return nil
+        }
+        
+        return viewControllers[index! + 1]
+    }
 
 }
 
